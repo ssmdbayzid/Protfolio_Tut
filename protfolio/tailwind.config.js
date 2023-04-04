348436e09509541b77cdad212886558b86c495f0
S S Md. Bayzid <96953200+ssmdbayzid@users.noreply.github.com>
2023-04-04 18:19:30 +0000

    tailwind added

--- protfolio/tailwind.config.js
+++ protfolio/tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  modu:'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blur: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
+        grey: "#ededed",
+        "deep-blue": "#010026",
+        "dark-blue": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)"
+      },
+      backgroundImage: (theme)=> ({
+        "gradiant-rainbow" : "linaer-CanvasGradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+        "gradiant-rainblue" : "linaer-CanvasGradient(90deg, #24CBFF 14.53%, #FC59FF 69.37%, #FFBD0C 117.73%)",
+      }),
+      fontFamily: {
+        playfair: ["Playfair", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
